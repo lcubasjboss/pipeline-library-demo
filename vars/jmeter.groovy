35ce303930cc3a1863f8200965f0a68d2a57fc4a
Luis Cubas <marcus_zero@hotmail.com>
2023-08-14 23:55:23 -0500

    Update jmeter.groovy

--- vars/jmeter.groovy
+++ vars/jmeter.groovy
@@ -7,7 +7,7 @@ def ejecutar() {
                 //Descargar archivo jmx y csv desde repositorio de Bitbucket de Performance
                 //Ejecutando script de JMeter por linea de comandos
     }
-                echo ${WORKSPACE}
+                echo "Llegamos"
                 // sh "jmeter.sh -n -t  ${WORKSPACE}/Pruebatecnica.jmx -l  ${WORKSPACE}/SC00_CrearReceta_UAT_5_VU_${timestamp}.jtl -JSC00.${threadscount} -Jlg.${threadsByStep} -Jlg.${timeSecByStep} -Jlg.${rampUpSec} -Jlg.${holdDuration} -Jlg.${threadsToStop} -Jlg.${shutdownTime} -e -o ${WORKSPACE}/SC00_CrearReceta_UAT_5_VU_${timestamp}_html"                 
                 // sh "cp -rf ${WORKSPACE}/SC00_CrearReceta_UAT_5_VU_${timestamp}_html output"
 
