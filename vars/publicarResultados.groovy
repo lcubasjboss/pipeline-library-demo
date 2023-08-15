7ae32933eb8ce47a8106236e452dde2a0d9c5812
Luis Cubas <marcus_zero@hotmail.com>
2023-08-15 00:41:28 -0500

    Update publicarResultados.groovy

--- vars/publicarResultados.groovy
+++ vars/publicarResultados.groovy
@@ -2,17 +2,15 @@ def publicarResultados() {
     println "Publicando Resultados .."
     //env.EMAIL_BUILD_STATUS = "FAILED"
     script {
-      echo "Publicando Resultados"
-      /*
-              publishHTML(target: [
-                    allowMissing: false,
-                    alwaysLinkToLastBuild: true,
-                    keepAll: true,
-                    reportDir: 'output',
-                    reportFiles: 'index.html',
-                    reportName: 'JMeter Report'
+      echo "Publicando Resultados - publicarResultados.groovy"
+      //            publishHTML(target: [
+      //              allowMissing: false,
+      //              alwaysLinkToLastBuild: true,
+      //              keepAll: true,
+      //              reportDir: 'output',
+      //              reportFiles: 'index.html',
+      //              reportName: 'JMeter Report'
                 ])
                 */
-    }
-               
+    }              
 }
