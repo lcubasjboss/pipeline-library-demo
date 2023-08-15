9f2ce45d375153d71937beca1430e0da8d9b978d
Luis Cubas <marcus_zero@hotmail.com>
2023-08-15 00:33:27 -0500

    Update jmeter.groovy

--- vars/jmeter.groovy
+++ vars/jmeter.groovy
@@ -11,6 +11,7 @@ def ejecutar() {
                 echo "${WORKSPACE}"
                 sh "ls -ltr"
             echo "${WORKSPACE}/SC00_CrearReceta_UAT_5_VU_${timestamp}.jtl"
+        sh "ls -ltr ${WORKSPACE}/01_Escenarios/"
     }
                 // sh "jmeter.sh -n -t  ${WORKSPACE}/Pruebatecnica.jmx -l  ${WORKSPACE}/SC00_CrearReceta_UAT_5_VU_${timestamp}.jtl -JSC00.${threadscount} -Jlg.${threadsByStep} -Jlg.${timeSecByStep} -Jlg.${rampUpSec} -Jlg.${holdDuration} -Jlg.${threadsToStop} -Jlg.${shutdownTime} -e -o ${WORKSPACE}/SC00_CrearReceta_UAT_5_VU_${timestamp}_html"                 
                 // sh "cp -rf ${WORKSPACE}/SC00_CrearReceta_UAT_5_VU_${timestamp}_html output"
