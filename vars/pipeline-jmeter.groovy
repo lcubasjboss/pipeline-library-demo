07394b5ee00bf937e1ba001ee7e847b9a094538a
Luis Cubas <marcus_zero@hotmail.com>
2023-08-17 22:35:16 -0500

    Update pipeline-jmeter.groovy

--- vars/pipeline-jmeter.groovy
+++ vars/pipeline-jmeter.groovy
@@ -1,13 +1,15 @@
-pipeline {
-  agent any
-  stages {
-    stage("Ejecución de JMeter") {
-      steps {
-        script{
-          echo "Iniciando Ejecución.."
-          //jmeter.ejecutar()
+// vars/pipeline-jmeter.groovy
+def call(Map pipelineParams) {
+    pipeline {
+        agent any
+        stages {
+            stage('Build') {
+               echo "Hola desde stage Build"
+            }
+
+            stage ('Test') {
+                echo "Hola desde stage Test"
+            }
         }
-      }
     }
-}
 }
