a92d69455286ff904a106ee72245025d8cb4154d
Luis Cubas <marcus_zero@hotmail.com>
2023-08-18 00:55:10 -0500

    Create pipelineJmeterParams.groovy

---
+++ vars/pipelineJmeterParams.groovy
@@ -0,0 +1,64 @@
+// vars/pipelineJmeterParams.groovy
+//def call(Map pipelineParams) {
+def call(body) {
+    //def pipelineParams= [:]
+    //body.resolveStrategy = Closure.DELEGATE_FIRST
+    //body.delegate = pipelineParams
+    //body()
+   echo "variables"
+     //pipelineParams.each { println(it) }
+     params.each {
+    println it.key + " = " + it.value
+  }
+  pipeline {
+    agent any
+    stages {
+     stage('Ejecución de JMeter') {
+       steps {
+            script{
+              echo "Iniciando Ejecución desde pipelineJmeter.groovy.."
+              //echo "Iniciando Ejecución.."
+              jmeter.ejecutar(params)
+            }
+       }
+     }
+     stage('Publicación de Resultados') {
+       steps {
+            script{
+              echo "Publicando Resultados desde pipelineJmeter.groovy.."
+              //echo "Publicando Resultados.."
+              jmeter.publicarResultados()
+            }
+       }
+     }
+    //stage('Subir resultados a Repositorio de Bitbucket') {
+    //   steps {
+    //       script{
+    //        echo "Subiendo resultados desde pipelineJmeter.groovy.."
+              echo "Subiendo resultados.."
+    //        jmeter.subirResultados()
+   // }
+  //}
+    //}
+     stage('Remover carpetas') {
+       steps {
+           script{
+             echo "Removiendo carpetas desde pipelineJmeter.groovy.."
+             echo "Removiendo carpetas.."
+             jmeter.removerCarpetas()
+           }
+       }
+    }
+  }
+  post {
+       always {
+            cleanWs(cleanWhenNotBuilt: false,
+                    deleteDirs: true,
+                    disableDeferredWipeout: true,
+                    notFailBuild: true,
+                    patterns: [[pattern: '.gitignore', type: 'INCLUDE'],
+                               [pattern: '.propsfile', type: 'EXCLUDE']])
+        }
+  }
+ }
+}
