6aa7451224ccaf5e9bb8309a6d0124985c4a7876
Luis Cubas <marcus_zero@hotmail.com>
2023-08-17 23:34:16 -0500

    Update pipelineJmeter.groovy

--- vars/pipelineJmeter.groovy
+++ vars/pipelineJmeter.groovy
@@ -1,24 +1,21 @@
 // vars/pipelineJmeter.groovy
-def call(Map pipelineParams) {
-//def call() {
+//def call(Map pipelineParams) {
+def call() {
     pipeline {
         agent any
         stages {
             stage('Build') {
                 steps {
                   echo "Hola desde stage Build"
-                  echo pipelineParams.name
-                  //echo "${pipelineParams.name}"
+                  //echo pipelineParams.name
                 }
             }
             stage ('Test') {
                 steps {
                   echo "Hola desde stage Test"
-                  echo pipelineParams.surname
-                  //echo "${pipelineParams.surname}"
+                  //echo pipelineParams.surname
                   //echo "Environment: ${pipelineParams.ENVIRONMENT}"
                  // echo "${pipelineParams.ENVIRONMENT}"
-                      //print "DEBUG: parameter isFoo = ${params.isFoo}"
                     script {
                       sayHello("FUNCIONANDO SAY HELLO")
                       sayHello.saludo("FUNCIONANDO SAY HELLO - saludo")
