f091870b3d05975c18e317ff63692b051b31440f
Luis Cubas <marcus_zero@hotmail.com>
2023-08-18 00:44:01 -0500

    Update pipelineJmeterParams.groovy

--- vars/pipelineJmeterParams.groovy
+++ vars/pipelineJmeterParams.groovy
@@ -27,7 +27,7 @@ def call(body) {
                   //echo "Environment: ${pipelineParams.ENVIRONMENT}"
                  // echo "${pipelineParams.ENVIRONMENT}"                  
                     println "DEBUG: parameter ENVIRONMENT = ${params.ENVIRONMENT}"
-                       println "DEBUG: parameter URL = ${params.URL}"
+                       println "DEBUG: parameter URL = ${params.link}"
                     script {
                       sayHello("FUNCIONANDO SAY HELLO")
                       sayHello.saludo("FUNCIONANDO SAY HELLO - saludo")
