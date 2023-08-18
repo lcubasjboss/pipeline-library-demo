c4ef7401d47a0104e5e78337d055e6e165967c4c
Luis Cubas <marcus_zero@hotmail.com>
2023-08-18 00:40:35 -0500

    Update pipelineJmeterParams.groovy

--- vars/pipelineJmeterParams.groovy
+++ vars/pipelineJmeterParams.groovy
@@ -2,12 +2,12 @@
 //def call(Map pipelineParams) {
 //def call()
 def call(body) {
-    def pipelineParams= [:]
-    body.resolveStrategy = Closure.DELEGATE_FIRST
-    body.delegate = pipelineParams
-    body()
+    //def pipelineParams= [:]
+    //body.resolveStrategy = Closure.DELEGATE_FIRST
+    //body.delegate = pipelineParams
+    //body()
    echo "variables"
-    pipelineParams.each { println(it) }
+   // pipelineParams.each { println(it) }
      params.each {
     println it.key + " = " + it.value
   }
@@ -25,11 +25,8 @@ def call(body) {
                   echo "Hola desde stage Test"
                   //echo pipelineParams.surname
                   //echo "Environment: ${pipelineParams.ENVIRONMENT}"
-                 // echo "${pipelineParams.ENVIRONMENT}"
-                    //echo pipelineParams.ENVIRONMENT
-                   
-                    print 'DEBUG: parameter ENVIRONMENT = ' + params.ENVIRONMENT
-                    print "DEBUG: parameter ENVIRONMENT = ${params.ENVIRONMENT}"
+                 // echo "${pipelineParams.ENVIRONMENT}"                  
+                    println "DEBUG: parameter ENVIRONMENT = ${params.ENVIRONMENT}"
                     script {
                       sayHello("FUNCIONANDO SAY HELLO")
                       sayHello.saludo("FUNCIONANDO SAY HELLO - saludo")
