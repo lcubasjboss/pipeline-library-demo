986d11a217b9345b942016fdb0e1e70523ca3629
Luis Cubas <marcus_zero@hotmail.com>
2023-08-17 23:48:16 -0500

    Update pipelineJmeter.groovy

--- vars/pipelineJmeter.groovy
+++ vars/pipelineJmeter.groovy
@@ -1,27 +1,55 @@
 // vars/pipelineJmeter.groovy
 //def call(Map pipelineParams) {
 def call() {
-    pipeline {
-        agent any
-        stages {
-            stage('Build') {
-                steps {
-                  echo "Hola desde stage Build"
-                  //echo pipelineParams.name
-                }
+  pipeline {
+    agent any
+    stages {
+     stage('Ejecución de JMeter') {
+       steps {
+            script{
+              echo "Iniciando Ejecución desde pipelineJmeter.groovy.."
+              //echo "Iniciando Ejecución.."
+              jmeter.ejecutar()
             }
-            stage ('Test') {
-                steps {
-                  echo "Hola desde stage Test"
-                  //echo pipelineParams.surname
-                  //echo "Environment: ${pipelineParams.ENVIRONMENT}"
-                 // echo "${pipelineParams.ENVIRONMENT}"
-                    script {
-                      sayHello("FUNCIONANDO SAY HELLO")
-                      sayHello.saludo("FUNCIONANDO SAY HELLO - saludo")
-                    }
-                }
+       }
+     }
+     stage('Publicación de Resultados') {
+       steps {
+            script{
+              echo "Publicando Resultados desde pipelineJmeter.groovy.."
+              //echo "Publicando Resultados.."
+              jmeter.publicarResultados()
             }
-        }
+       }
+     }
+    //stage('Subir resultados a Repositorio de Bitbucket') {
+    //   steps {
+    //       script{
+    //        echo "Subiendo resultados desde pipelineJmeter.groovy.."
+              echo "Subiendo resultados.."
+    //        jmeter.subirResultados()
+   // }
+  //}
+    //}
+     stage('Remover carpetas') {
+       steps {
+           script{
+             echo "Removiendo carpetas desde pipelineJmeter.groovy.."
+             echo "Removiendo carpetas.."
+             jmeter.removerCarpetas()
+           }
+       }
     }
+  }
+  post {
+       always {
+            cleanWs(cleanWhenNotBuilt: false,
+                    deleteDirs: true,
+                    disableDeferredWipeout: true,
+                    notFailBuild: true,
+                    patterns: [[pattern: '.gitignore', type: 'INCLUDE'],
+                               [pattern: '.propsfile', type: 'EXCLUDE']])
+        }
+  }
+ }
 }
