0f603b5f1d36b96a88a934ad80ab5842886fc7d7
Luis Cubas <marcus_zero@hotmail.com>
2023-08-18 00:10:19 -0500

    Create pipelineJmeterParams.groovy

---
+++ vars/pipelineJmeterParams.groovy
@@ -0,0 +1,33 @@
+// vars/pipelineJmeterTestParams.groovy
+//def call(Map pipelineParams) {
+//def call()
+def call(body) {
+    def pipelineParams= [:]
+    body.resolveStrategy = Closure.DELEGATE_FIRST
+    body.delegate = pipelineParams
+    body()
+    pipelineParams.each { println(it) }
+    pipeline {
+        agent any
+        stages {
+            stage('Build') {
+                steps {
+                  echo "Hola desde stage Build"
+                  //echo pipelineParams.name
+                }
+            }
+            stage ('Test') {
+                steps {
+                  echo "Hola desde stage Test"
+                  //echo pipelineParams.surname
+                  //echo "Environment: ${pipelineParams.ENVIRONMENT}"
+                 // echo "${pipelineParams.ENVIRONMENT}"
+                    script {
+                      sayHello("FUNCIONANDO SAY HELLO")
+                      sayHello.saludo("FUNCIONANDO SAY HELLO - saludo")
+                    }
+                }
+            }
+        }
+    }
+}
