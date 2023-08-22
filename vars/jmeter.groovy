a16799aa26fe01610e5396bd8e9926a43cbdbdef
Luis Cubas <marcus_zero@hotmail.com>
2023-08-21 22:45:05 -0500

    Update jmeter.groovy

--- vars/jmeter.groovy
+++ vars/jmeter.groovy
@@ -1,32 +1,31 @@
 //def ejecutar() {                
 def ejecutar(params) {                
-    println "Checando variables que vienen de Jenkins Job"
-      params.each {
+  println "Checando variables que vienen de Jenkins Job"
+  params.each {
     println it.key + " = " + it.value
   }
-   params.each {param ->
-  println " '${param.key.trim()}' -> '${param.value.trim()}' "
+  params.each {param ->
+    println " '${param.key.trim()}' -> '${param.value.trim()}' "
+  }
+  print 'DEBUG: parameter TC = ' + params.threadscount
+  print "DEBUG: parameter TC = ${params.threadscount}"    
+  println env.branch
+  def myvariables = getBinding().getVariables()
+for (v in myvariables) {
+   echo "${v} " + myvariables.get(v)
 }
-    
-    print 'DEBUG: parameter TC = ' + params.threadscount
-print "DEBUG: parameter TC = ${params.threadscount}"
-
-    
-    //println System.getenv("threadscount") 
-    //def tc = System.getenv("threadscount")
-    //println tc
-    println "Ejecutando JMeter.."
-    script {
-        fecha = new Date()
-        timestamp = fecha.format("yyMMdd.HHmm", TimeZone.getTimeZone('UTC'))
-        sh("mkdir ${WORKSPACE}/SC00_CrearReceta_UAT_5_VU_${timestamp}_html")
+  println "Ejecutando JMeter.."
+  script {
+       fecha = new Date()
+       timestamp = fecha.format("yyMMdd.HHmm", TimeZone.getTimeZone('UTC'))
+       sh("mkdir ${WORKSPACE}/SC00_CrearReceta_UAT_5_VU_${timestamp}_html")
         //sh("touch ${WORKSPACE}/SC00_CrearReceta_UAT_5_VU_${timestamp}_html/test")
-    }
+  }
     //sh "jmeter.sh -n -t  ${WORKSPACE}/01_Escenarios/Pruebatecnica.jmx -l  ${WORKSPACE}/SC00_CrearReceta_UAT_5_VU_${timestamp}.jtl -JSC00.${threadscount} -Jlg.${threadsByStep} -Jlg.${timeSecByStep} -Jlg.${rampUpSec} -Jlg.${holdDuration} -Jlg.${threadsToStop} -Jlg.${shutdownTime} -e -o ${WORKSPACE}/SC00_CrearReceta_UAT_5_VU_${timestamp}_html"                 
     //Descomentar
-    sh "jmeter.sh -n -t  ${WORKSPACE}/01_Escenarios/Pruebatecnica.jmx -l  ${WORKSPACE}/SC00_CrearReceta_UAT_5_VU_${timestamp}.jtl -JSC00.${params.threadscount} -Jlg.${params.threadsByStep} -Jlg.${params.timeSecByStep} -Jlg.${params.rampUpSec} -Jlg.${params.holdDuration} -Jlg.${params.threadsToStop} -Jlg.${params.shutdownTime} -e -o ${WORKSPACE}/SC00_CrearReceta_UAT_5_VU_${timestamp}_html"                 
-    sh "cp -rf ${WORKSPACE}/SC00_CrearReceta_UAT_5_VU_${timestamp}_html output"
-    sh "ls -ltr output"
+    //sh "jmeter.sh -n -t  ${WORKSPACE}/01_Escenarios/Pruebatecnica.jmx -l  ${WORKSPACE}/SC00_CrearReceta_UAT_5_VU_${timestamp}.jtl -JSC00.${params.threadscount} -Jlg.${params.threadsByStep} -Jlg.${params.timeSecByStep} -Jlg.${params.rampUpSec} -Jlg.${params.holdDuration} -Jlg.${params.threadsToStop} -Jlg.${params.shutdownTime} -e -o ${WORKSPACE}/SC00_CrearReceta_UAT_5_VU_${timestamp}_html"                 
+    //sh "cp -rf ${WORKSPACE}/SC00_CrearReceta_UAT_5_VU_${timestamp}_html output"
+    //sh "ls -ltr output"
 }
 
 def publicarResultados() {                
